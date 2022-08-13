6fff70fa1b7e2f7302790405c3a95822ae7e2aa7
Amol Tatkare <amol.tatkare@pfizer.com>
2022-08-13 20:45:00 +0530

    Initial reusable code for AWS EC2 using Terraform

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "ami" {
+  description = "The AMI to run on EC2"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance Type of EC2"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "VPC Subnet ID"
+  type        = string
+}
+
+variable "security_group" {
+  description = "Security group ID"
+  type        = string
+}
+
+variable "resource_tags" {
+  description = "Tags to set for all resources"
+  type        = map(string)
+  default     = {
+    environment = "dev"
+  }
+}
